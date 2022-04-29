1b7414d386a3d2fecd25dcada04461ef5b7d5f0e
Matt Quayle <mattgq2@gmail.com>
2022-04-28 19:50:11 -0600

    Initial commit

---
+++ passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy, //DEFINES BASIC HTTP AUTHENTICATION
+  Models = require('./models.js'),
+  passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password'
+}, (username, password, callback) => {
+  console.log(username + ' ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
+    if (!user) {
+      console.log('incorrect username');
+      return callback(null, false, { message: 'Incorrect username or password.' });
+    }
+
+    //if(!user.validatePassword(password)){
+    //  console.log('Incorrect password');
+    //  return callback(null, false, {message: 'Incorrect password.'});
+    //}
+
+    console.log('Finished');
+    return callback(null, user);
+  });
+}));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id).then((user) => {
+    return callback(null, user);
+  })
+    .catch((error) => {
+      return callback(error)
+    });
+}));
