4172d93427d03e30382a443e675eaa4bd58e8ead
Matt Quayle <mattgq2@gmail.com>
2022-09-20 19:34:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -202,11 +202,11 @@ app.delete('/users/:Username', (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 8080);
-//const port = process.env.PORT || 8080;
-//app.listen(port, '0.0.0.0', () => {
-//  console.log('Listening on Port ' + port);
-//});
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
+});
 
 //app.listen(8080, () => {
 //  console.log('App listening on port 8080.');
